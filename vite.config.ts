0a53dcbbfef3aaf1b5599765e9a5c062dfbf1222
Shikhar Srivastava <shikhars2006@gmail.com>
2026-08-28 00:00:59 +0530

    more config changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/shikhar-srivastava-16.github.io/', // must match your repo name
+    base: '/', // must match your repo name
     plugins: [react()],
 })
